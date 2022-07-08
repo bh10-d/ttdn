632aded485a9127be268626445694b9a9f5ecde0
bh10-d <duchieubui25012002@gmail.com>
2022-07-08 07:03:59 +0700

    .

--- time.py
+++ time.py
@@ -4,12 +4,20 @@
 # 		nf.write(x[x.find(':')+1:x.find(':')+9]+"\n")
 # 		print(x[x.find(':')+1:x.find(':')+9])
 
+import geocoder
 
-def clog(file):
-	nf = open("test.txt", "w")
+# def clog(file):
+# 	nf = open("test.txt", "w")
+# 	for x in file:
+# 		# nf.write(x[:x.find('"')]+"\n")
+# 		print(x[:x.find('"')]+"\n")
+
+def location(file):
+	nf = open("location.txt","w")
 	for x in file:
-		# nf.write(x[:x.find('"')]+"\n")
-		print(x[:x.find('"')]+"\n")
+		ip = geocoder.ip(x)
+		nf.write("IP: "+x+"- Location: "+str(ip.city.encode("utf-8")))
+		# print(ip.city)
 
-f = open("mobilog.txt", "r")
-clog(f)
+f = open("count.txt", "r")
+location(f)
